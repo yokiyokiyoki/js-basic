497b85a85319bcd625210365b8f5aa85d6342331
李友杰 <liyoujie@liyoujiedeMacBook-Pro.local>
2018-02-28 16:01:12 +0800

    less拓展

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from "./router";
 import ElementUI from "element-ui";
 import "normalize.css";
 import "element-ui/lib/theme-chalk/index.css";
+import "@/less/app";
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
