283b8277097efd94bfd3b0a050cdefdc858d59da
李友杰 <liyoujie@liyoujiedeMacBook-Pro.local>
2018-02-28 15:47:54 +0800

    normalize

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App";
 import router from "./router";
 
 import ElementUI from "element-ui";
+import "normalize.css";
 import "element-ui/lib/theme-chalk/index.css";
 
 Vue.use(ElementUI);
