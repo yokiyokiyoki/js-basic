c6f08d24381fab2fab96fb47b22b4beb1ec25dc8
李友杰 <liyoujie@liyoujiedeMacBook-Pro.local>
2018-02-28 16:44:09 +0800

    提交第一个简介

--- src/pages/array/components/intro.vue
+++ src/pages/array/components/intro.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="intro">
-        1
+    <el-card class="box-card">
+        <div slot='header'>
+            <div class="title">
+                - 本质是对象，引用类型
+            </div>
+        </div>   
+    </el-card> 
   </div>
 </template>
 <script>
 export default {};
 </script>
+<style lang="less" scoped>
+.box-card {
+  .title {
+    text-align: left;
+  }
+}
+</style>
