9b737aa50594e3e07c72e1d4a458a6df8e91fbc9
youjie <youjie@hudongpai.com>
2018-04-03 19:17:25 +0800

    数组去重

--- src/pages/array/components/repeat.vue
+++ src/pages/array/components/repeat.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="repeat"></div>
+  <div class="repeat">
+      <el-card class="box-card">
+        <div slot='header'>
+            <div class="title">
+                - 数组去重
+            </div>
+        </div>
+        <div class="item">
+            
+        </div>   
+    </el-card>
+  </div>
 </template>
 <script>
+export default {};
 </script>
+
